b77683818da2d3fbe92f2fdb9d893d46bfbb9355
vijaydalvi2026 <vijaydalvi2026@gmail.com>
2026-08-05 20:17:48 +0530

    Machine-Learning-Project

---
+++ ml.py
@@ -0,0 +1,16 @@
+%%capture
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import nltk
+import string
+import warnings
+from scipy.stats import pearsonr
+from nltk.corpus import stopwords
+from wordcloud import WordCloud
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+nltk.download('stopwords')
+warnings.filterwarnings('ignore')
